380f21df79e0847854a3bec50fa5b184d3c05f5c
NadineBestard <nadine.bestard@ed.ac.uk>
2022-09-14 18:15:35 +0100

    apply functions to young data

--- src/plots/FC_clusters_plot_ambient.R
+++ src/plots/FC_clusters_plot_ambient.R
@@ -1,107 +1,122 @@
-#SCRIPT FOR PLOT WITH FC ON Y AXIS, GROUPS IN X AXIS
+# SCRIPT FOR PLOT WITH FC ON Y AXIS, GROUPS IN X AXIS
 
 ## set up ---
 
-library(here) #reproducible paths
+library(here) # reproducible paths
 library(tidyverse) # manipulate dfs and ggplots
 
 # functions
-plot_FC_bycluster_ambient <- function(de_results,
-                                    ambient = TRUE,
-                                    ambient_threshold = 0.1,
-                                    FDR_threshold = 0.05,
-                                    cols = c("darkred", "darkgreen", "#999999" ),
-                                    data = FALSE){
+plot_FC_bycluster <- function(de_results,
+                                      ambient = FALSE,
+                                      ambient_threshold = 0.1,
+                                      FDR_threshold = 0.05,
+                                      logFC_threshold = 0,
+                                      cols = c("darkred", "darkgreen", "#999999"),
+                                      data = FALSE) {
+  
   # transform all the DFrame to a standart df format
-  de_results_dfs <- lapply(de_results, 
-                           function(x) {
-                             na.omit(as.data.frame(x)) %>% 
-                               rownames_to_column("gene")
-                           }
+  de_results_dfs <- lapply(
+    de_results,
+    function(x) {
+      na.omit(as.data.frame(x)) %>%
+        rownames_to_column("gene")
+    }
   )
   # merge all the dfs from the list in a single big dataframe
-  de_results_df <- bind_rows(de_results_dfs, .id = "cluster") %>% 
+  de_results_df <- bind_rows(de_results_dfs, .id = "cluster") %>%
     # add column that indicates if the result is significant
-    mutate(Significant = ifelse(FDR < FDR_threshold, 
-                                yes = ifelse(logFC > 0, "upregulated", "downregulated"),
-                                no = "not-significant")) 
-  
-  if(ambient==TRUE){
+    mutate(Significant = ifelse(FDR < FDR_threshold,
+      yes = ifelse(logFC > logFC_threshold, "upregulated", "downregulated"),
+      no = "not-significant"
+    ))
+
+  if (ambient == TRUE) {
     # filter the genes that are more than 10% ambient (or other threshold)
-    de_results_df <- de_results_df %>% 
+    de_results_df <- de_results_df %>%
       filter(minAmbient < ambient_threshold)
   }
-  
-  if(data == FALSE){
+
+  if (data == FALSE) {
     ## plot ---
-    de_results_df %>% 
+    de_results_df %>%
       # order the levels to display in correct order in plot
-      mutate(Significant = forcats::fct_relevel(Significant, c("upregulated", "downregulated", "not-significant"))) %>% 
-      # mutate(cluster = forcats::fct_relevel(cluster, c("Astrocyte_1", "Astrocyte_2", "Astrocyte_3", "OligoAstro", "Oligo_1", "Oligo_2", "OPCs", "mNeurons", "iNeurons & NRPs", "Lymphocytes", "BAMs", "DCs",  "Endothelial", "fEndothelia", "Mural_cells", "ChP_epithelial"))) %>% 
+      mutate(Significant = forcats::fct_relevel(Significant, c("upregulated", "downregulated", "not-significant"))) %>%
+      # mutate(cluster = forcats::fct_relevel(cluster, c("Astrocyte_1", "Astrocyte_2", "Astrocyte_3", "OligoAstro", "Oligo_1", "Oligo_2", "OPCs", "mNeurons", "iNeurons & NRPs", "Lymphocytes", "BAMs", "DCs",  "Endothelial", "fEndothelia", "Mural_cells", "ChP_epithelial"))) %>%
       # sort so the significant values are plotted on top of the non significants
-      arrange(desc(Significant)) %>% 
-    ggplot(mapping = aes(x=cluster, y=logFC, color=Significant)) + 
-      geom_jitter() + 
+      arrange(desc(Significant)) %>%
+      ggplot(mapping = aes(x = cluster, y = logFC, color = Significant)) +
+      geom_jitter() +
       scale_colour_manual(values = cols) +
-      theme_minimal() + 
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-            legend.title = element_blank()) +
+      theme_minimal() +
+      theme(
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        legend.title = element_blank()
+      ) +
       ylab("logFC fire-mice ko to wt") +
       xlab(element_blank())
-  }else{
+  } else {
     de_results_df
   }
 }
 
 get_ambient_genes <- function(de_results,
                               ambient_threshold = 0.1,
-                              FDR_threshold = 0.05){
+                              FDR_threshold = 0.05) {
   # transform all the DFrame to a standart df format
-  de_results_dfs <- lapply(de_results, 
-                           function(x) {
-                             na.omit(as.data.frame(x)) %>% 
-                               rownames_to_column("gene")
-                           }
+  de_results_dfs <- lapply(
+    de_results,
+    function(x) {
+      na.omit(as.data.frame(x)) %>%
+        rownames_to_column("gene")
+    }
   )
   # merge all the dfs from the list in a single big dataframe
-  de_results_df <- bind_rows(de_results_dfs, .id = "cluster") %>% 
-    filter(minAmbient > ambient_threshold) %>% 
+  de_results_df <- bind_rows(de_results_dfs, .id = "cluster") %>%
+    filter(minAmbient > ambient_threshold) %>%
     # add column that indicates if the result is significant
-    mutate(Significant = ifelse(FDR < FDR_threshold, 
-                                yes = ifelse(logFC > 0, "upregulated", "downregulated"),
-                                no = "not-significant")) %>% 
-    # mutate(cluster = forcats::fct_relevel(cluster, c("Astrocyte_1", "Astrocyte_2", "Astrocyte_3", "OligoAstro", "Oligo_1", "Oligo_2", "OPCs", "mNeurons", "iNeurons & NRPs", "Lymphocytes", "BAMs", "DCs",  "Endothelial", "fEndothelia", "Mural_cells", "ChP_epithelial"))) %>% 
+    mutate(Significant = ifelse(FDR < FDR_threshold,
+      yes = ifelse(logFC > 0, "upregulated", "downregulated"),
+      no = "not-significant"
+    )) %>%
+    # mutate(cluster = forcats::fct_relevel(cluster, c("Astrocyte_1", "Astrocyte_2", "Astrocyte_3", "OligoAstro", "Oligo_1", "Oligo_2", "OPCs", "mNeurons", "iNeurons & NRPs", "Lymphocytes", "BAMs", "DCs",  "Endothelial", "fEndothelia", "Mural_cells", "ChP_epithelial"))) %>%
     # sort so the significant values are plotted on top of the non significants
     arrange(desc(Significant))
 }
+
+
 # project
 project <- "fire-mice"
 
 
-## import and transform data ---
+# loop through the 3 genotype comparisons
 
-# this is the output from edgeR for sc
-# a list with the DE results, each element named as one of the clusters and 
-# contain a DFrame with the DE for that cluster
-de_results <- readRDS(here("processed", project, "DE_results_ambient_edgeR_KO.RDS"))
+for (gnt in c("KO", "HET", "KOvsHET")) {
 
 
+  # load output from edgeR for each genoype
 
-plot_FC_bycluster_ambient(de_results)
-ggsave(here("outs", project, "DE_edgeR", "plots", "FC_clusters_minAmbient.pdf"), 
-       height = 7, width = 10)
+  de_results_gnt <- readRDS(here("processed", project, paste0("DE_results_ambient_edgeR_", gnt, ".RDS")))
 
-de_results_df <- plot_FC_cluster_ambient(de_results, data = TRUE)
-write.csv(filter(de_results_df, Significant != "not-significant"),
-          here("outs", project, "DE_edgeR", "de_results_KO", "de_results_KO_combined_significant_ambientremoved.csv" ),
-          row.names = FALSE)
+  # the output from edgeR for sc is
+  # a list with the DE results, each element named as one of the clusters and
+  # contain a DFrame with the DE for that cluster
 
+  plot_FC_bycluster(de_results_gnt, ambient=TRUE, ambient_threshold = 0.25)
+  ggsave(here("outs", project, "DE_edgeR", "plots", paste0("FC_clusters_", gnt, ".pdf")),
+    height = 7, width = 10
+  )
 
-## save csv with deleted genes
-# merge all the dfs from the list in a single big dataframe
-de_results_df_ambient <- get_ambient_genes(de_results)
+  de_results_df <- plot_FC_bycluster(de_results_gnt, ambient=TRUE, data = TRUE)
+  write.csv(filter(de_results_df, Significant != "not-significant"),
+    here("outs", project, "DE_edgeR", paste0("de_results_", gnt), paste0("de_results_", gnt, "_combined_significant_ambientremoved.csv")),
+    row.names = FALSE
+  )
 
-write.csv(filter(de_results_df_ambient, Significant != "not-significant"),
-          here("outs", project, "DE_edgeR", "de_results_KO", "de_results_KO_combined_significant_onlyambient.csv" ),
-          row.names = FALSE)
-  
+  ## save csv with deleted genes
+  de_results_df_ambient <- get_ambient_genes(de_results_gnt)
+
+  write.csv(filter(de_results_df_ambient, Significant != "not-significant"),
+    here("outs", project, "DE_edgeR", paste0("de_results_", gnt), paste0("de_results_", gnt, "_combined_significant_onlyambient.csv")),
+    row.names = FALSE
+  )
+}
